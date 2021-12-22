25e444bb47b77ada920782f44b3177112718b596
MariyaGordeeva <mariya.gordeeva@devexpress.com>
2021-12-22 17:06:07 +0300

    DevExpress Chips for .NET MAUI Preview 11

---
+++ CS/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevExpress.Maui.Editors;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace ChipsExample {
+    public partial class MainPage : ContentPage {
+        const int animationDuration = 600;
+
+        ViewModel VM { get; }
+
+        public MainPage() {
+            InitializeComponent();
+            BindingContext = VM = new ViewModel();
+            colorChoiceChipGroup.SelectionChanged += OnColorChanged;
+        }
+
+        async void OnColorChanged(object sender, EventArgs e) {
+            superhero.TranslationX = 0;
+            superhero.CancelAnimations();
+            double translationX = superhero.Width;
+            await Task.WhenAll(
+                superhero.FadeTo(0, animationDuration, Easing.Linear),
+                superhero.TranslateTo(translationX, superhero.Y, animationDuration, Easing.CubicInOut)
+                );
+
+            VM.UpdateSuperhero();
+            superhero.TranslationX = -translationX;
+
+            await Task.WhenAll(
+                superhero.FadeTo(1, animationDuration, Easing.Linear),
+                superhero.TranslateTo(0, superhero.Y, animationDuration, Easing.CubicInOut)
+                );
+        }
+    }
+}
+
---
+++ CS/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.Maui.Controls;
+
+namespace ChipsExample {
+    public class ViewModel : INotifyPropertyChanged {
+        string selectedSize;
+        string selectedSuperhero;
+
+        public IList<string> Sizes { get; }
+        public IList<string> Superheroes { get; }
+        public int SelectedColorIndex { get; set; }
+
+        public string SelectedSize {
+            get => this.selectedSize;
+            set {
+                this.selectedSize = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SelectedSuperhero {
+            get => this.selectedSuperhero;
+            set {
+                this.selectedSuperhero = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModel() {
+            Sizes = new List<string>() { "S", "M", "L", "XL", "XXL", "XXXL" };
+            Superheroes = new List<string>() {
+                "superhero_red",
+                "superhero_orange",
+                "superhero_yellow",
+                "superhero_green",
+                "superhero_blue",
+                "superhero_purple"
+            };
+            SelectedColorIndex = 1;
+            SelectedSize = Sizes[2];
+            UpdateSuperhero();
+        }
+
+        public void UpdateSuperhero() {
+            SelectedSuperhero = (SelectedColorIndex == -1) ? null : Superheroes[SelectedColorIndex];
+        }
+
+        void OnPropertyChanged([CallerMemberName] string name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
+
